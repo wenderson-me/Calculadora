b454ea9dce051fe8bbd8eaa5c084ffd5b246a9aa
wenderson-me <wendrson22@gmail.com>
2020-04-20 15:46:26 -0300

    ajustando botão de ponto

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -45,6 +45,8 @@ class CalcController {
     clearAll() {
 
         this._operation = [];
+        this._lastNumber = '';
+        this._lastOperator = '';
 
         this.setLastNumberToDisplay();
 
@@ -180,10 +182,6 @@ class CalcController {
 
                 this.setLastOperation(value);
 
-            } else if (isNaN(value)) {
-
-                console.log('Outra coisa', value);
-
             } else {
 
                 this.pushOperation(value);
@@ -202,7 +200,7 @@ class CalcController {
             } else {
 
                 let newValue = this.getLastOperation().toString() + value.toString();
-                this.setLastOperation(parseInt(newValue));
+                this.setLastOperation(parseFloat(newValue));
 
                 //atualizar display
                 this.setLastNumberToDisplay();
@@ -216,6 +214,20 @@ class CalcController {
     setError() {
 
         this.displayCalc = "Error";
+
+    }
+
+    addDot() {
+
+        let lastOperation  = this.getLastOperation();
+
+        if (this.isOperation(lastOperation) || !lastOperation) {
+            this.pushOperation('0.');
+        } else {
+            this.setLastOperation(lastOperation.toString() + '.');
+        }
+
+        this.setLastNumberToDisplay();
 
     }
 
@@ -252,7 +264,7 @@ class CalcController {
                 break;
 
             case 'ponto':
-                this.addOperation('.');
+                this.addDot();
                 break;
 
             case '0':
